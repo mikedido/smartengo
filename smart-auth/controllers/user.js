e8569a09c967d82659be7e3fc04fedcbb38b63f0
mahdi gueffaz <macbook@MacBook-Pro-de-macbook.local>
2021-09-26 23:19:02 +0200

    init the router

---
+++ smart-auth/controllers/user.js
@@ -0,0 +1,4 @@
+module.exports.signup = (req, res, next) => {
+    
+    res.status(200).json('test ok');
+}
--- smart-auth/router/user.js
+++ smart-auth/router/user.js
@@ -4,18 +4,6 @@ const auth = require('../middleware/auth')
 const userController = require('../controllers/user');
 
 
-router.post('/signup', userController.signup);
-
-router.post('/signin', userController.login);
-
-router.get('/', auth, userController.getAllUsers);
-
-router.get('/:userId', auth, userController.getUser);
-
-router.get('/', auth, userController.getAllUsers);
-
-router.delete('/:userId', auth, userController.delete);
-
-router.put('/:userId', auth, userController.update);
+router.get('/signup', userController.signup);
 
 module.exports = router;
