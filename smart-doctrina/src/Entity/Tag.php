e832554b930b71e41134ed7cc776b3ec1ceed93b
mahdi gueffaz <macbook@MacBook-Pro-de-macbook.local>
2021-10-24 14:59:23 +0200

    Add relation between the entities

--- smart-doctrina/src/Entity/Tag.php
+++ smart-doctrina/src/Entity/Tag.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -22,5 +24,42 @@ class Tag
      * @ORM\Column(type="text", nullable=true)
      */
     private $title;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Article::class, mappedBy="tag")
+     */
+    private $articles;
+
+    public function __construct()
+    {
+        $this->articles = new ArrayCollection();
+    }
+
+    /**
+     * @return Collection|Article[]
+     */
+    public function getArticles(): Collection
+    {
+        return $this->articles;
+    }
+
+    public function addArticle(Article $article): self
+    {
+        if (!$this->articles->contains($article)) {
+            $this->articles[] = $article;
+            $article->addTag($this);
+        }
+
+        return $this;
+    }
+
+    public function removeArticle(Article $article): self
+    {
+        if ($this->articles->removeElement($article)) {
+            $article->removeTag($this);
+        }
+
+        return $this;
+    }
 
 }
