4a307e38925ea506621d04aa8039dea88dcd229b
mahdi gueffaz <macbook@MacBook-Pro-de-macbook.local>
2021-11-07 19:56:32 +0100

    Add login page

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="Connexion">
+    <div>
+      Login page
+      <Login />
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Login from '@/components/Login.vue'
+
+export default {
+  name: 'Connexion',
+  components: {
+    Login
+  }
+}
+</script>
