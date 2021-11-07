822e90d749e967e5941871ad214a14b9297a6c98
mahdi gueffaz <macbook@MacBook-Pro-de-macbook.local>
2021-11-07 19:57:09 +0100

    Add register page

---
+++ frontend/src/views/Registration.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="Enregistrement">
+    <div>
+      Register page
+      <Registration />
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Registration from '@/components/Registration.vue'
+
+export default {
+  name: 'Enregistrement',
+  components: {
+    Registration
+  }
+}
+</script>
